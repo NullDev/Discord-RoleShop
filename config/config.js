e7c708aaa1ad9714c14d53b4b79d09710b3a47c4
NullDev (Shadow) <chris@nulldev.org>
2023-04-30 06:00:32 +0200

    added configs

---
+++ config/config.js
@@ -0,0 +1,40 @@
+import fs from "node:fs/promises";
+import Log from "../src/util/log.js";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+try {
+    await fs.access("./config/config.js");
+}
+catch (error){
+    console.log(error);
+    Log.error("Config file not found. To create one, either copy 'config.template.js' and rename it to 'config.custom.js' or run 'npm run config'.");
+    process.exit(1);
+}
+
+try {
+    await fs.access("./config/config.template.js");
+}
+catch (error){
+    Log.error("Config template file not found. This is needed to read default values. Please re-clone the repository.");
+    process.exit(1);
+}
+
+// @ts-ignore
+const configCustom = (await import("./config.custom.js")).default;
+const configBase = (await import("./config.template.js")).default;
+
+const packageJSON = JSON.parse(await fs.readFile("./package.json", "utf-8"));
+
+export const meta = {
+    getVersion: () => packageJSON.version,
+    getName: () => packageJSON.name,
+    getAuthor: () => packageJSON.author,
+};
+
+export const config = {
+    ...configBase,
+    ...configCustom,
+};
---
+++ config/config.template.js
@@ -0,0 +1,9 @@
+export default {
+    discord: {
+        bot_token: "",
+        bot_status: "Couting messages...",
+    },
+    bot_settings: {
+        booster_multiplier: 2, // set this to 1 to disable
+    },
+};
