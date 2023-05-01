2bcf728fb636c20db249da6dd2e91fe5f6a7fd0d
NullDev (Shadow) <chris@nulldev.org>
2023-05-01 06:40:41 +0200

    edited template

--- config/config.template.js
+++ config/config.template.js
@@ -1,9 +1,10 @@
 export default {
     discord: {
         bot_token: "",
-        bot_status: "Couting messages...",
+        bot_status: "Usage: /rs-help",
     },
     bot_settings: {
+        slash_command_prefix: "rs",
         booster_multiplier: 2, // set this to 1 to disable
     },
 };
