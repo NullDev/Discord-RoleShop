7762c39ac393520419728d6ac6491375f9c44891
NullDev (Shadow) <chris@nulldev.org>
2023-05-01 07:59:18 +0200

    added annotations

--- src/service/client.js
+++ src/service/client.js
@@ -1,6 +1,22 @@
 import { Client, Collection } from "discord.js";
 
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+/**
+ *  Discord Client
+ *
+ * @class DiscordClient
+ * @extends {Client}
+ */
 class DiscordClient extends Client {
+    /**
+     *  Creates an instance of DiscordClient.
+     *
+     * @param {*} options
+     * @memberof DiscordClient
+     */
     constructor(options){
         super(options);
         this.commands = new Collection();
