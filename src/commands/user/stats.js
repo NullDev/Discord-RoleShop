bb40fa620c5d748a45cbb2c4d92a3fa7c3c74bca
NullDev (Shadow) <chris@nulldev.org>
2023-05-01 10:25:02 +0200

    added stats command

--- src/commands/user/stats.js
+++ src/commands/user/stats.js
@@ -1,5 +1,46 @@
+import path from "node:path";
+import { SlashCommandBuilder } from "discord.js";
+import { QuickDB } from "quick.db";
+import { config } from "../../../config/config.js";
+import __ from "../../service/i18n.js";
+
 // ========================= //
 // = Copyright (c) NullDev = //
 // ========================= //
 
-export default {};
+/* eslint-disable curly */
+
+const db = new QuickDB({
+    filePath: path.resolve("./data/users.sqlite"),
+});
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName(`${config.bot_settings.slash_command_prefix}-stats`)
+        .setDescription("Shows your or another users stats.")
+        .setDMPermission(false)
+        .addUserOption((option) =>
+            option.setName("user")
+                .setDescription("The user to show stats for")
+                .setRequired(false)),
+    /**
+     * @param {import("discord.js").CommandInteraction} interaction
+     */
+    async execute(interaction){
+        const user = interaction.options.get("user");
+        if (user?.user?.bot) return await interaction.reply({
+            content: await __("errors.stats_on_bot")(interaction.guildId),
+            ephemeral: true,
+        });
+
+        const userid = user?.user?.id || interaction.user.id;
+        const pointsKey = `guild-${interaction.guildId}.user-${userid}.points`;
+
+        const points = await db.get(pointsKey) || 0;
+
+        if (!user?.user?.id){
+            return await interaction.reply(await __("replies.stats_you", points)(interaction.guildId));
+        }
+        return await interaction.reply(await __("replies.stats_other", user.user.tag, points)(interaction.guildId));
+    },
+};
