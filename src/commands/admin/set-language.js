5b9b41cc2810b26ed92da3a2ea3eeedc886062e9
NullDev (Shadow) <chris@nulldev.org>
2023-05-01 09:28:47 +0200

    added set language command

---
+++ src/commands/admin/set-language.js
@@ -0,0 +1,34 @@
+import { SlashCommandBuilder } from "discord.js";
+// import { config } from "../../../config/config.js";
+import { QuickDB } from "quick.db";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+const db = new QuickDB();
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("rs-set-language")
+        .setDescription("Sets the Server Language for the bot.")
+        .setDMPermission(false)
+        .addStringOption((option) =>
+            option.setName("language")
+                .setDescription("Server language")
+                .setRequired(true)
+                .addChoices(
+                    { name: "German", value: "de" },
+                    { name: "English", value: "en" },
+                )),
+
+    /**
+     * @param {import("discord.js").CommandInteraction} interaction
+     */
+    async execute(interaction){
+        const lang = interaction.options.get("language");
+        if (!lang) return await interaction.reply({ content: "Invalid language", ephemeral: true });
+        await db.set(`${interaction.guildId}.locale`, lang.value || "en");
+        return await interaction.reply({ content: "Language set to: " + lang.value, ephemeral: true });
+    },
+};
