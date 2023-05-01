99c57209897129fab03987b10caca486743f1077
NullDev (Shadow) <chris@nulldev.org>
2023-05-01 15:16:34 +0200

    Added help commands

---
+++ src/commands/admin/admin-help.js
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import { config } from "../../../config/config.js";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName(`${config.bot_settings.slash_command_prefix}-admin-help`)
+        .setDescription("Shows an overview of all admin commands.")
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+    /**
+     * @param {import("discord.js").CommandInteraction} interaction
+     */
+    async execute(interaction){
+        const userCommands = /** @type {import("../../service/client.js").default} */ (interaction.client)
+            .commands.filter(cmd => cmd.data.default_member_permissions !== undefined);
+
+        return await interaction.reply({
+            content: userCommands.map((cmd) => `**/${cmd.data.name}** - ${cmd.data.description}`).join("\n"),
+            ephemeral: true,
+        });
+    },
+};
---
+++ src/commands/user/help.js
@@ -0,0 +1,25 @@
+import { SlashCommandBuilder } from "discord.js";
+import { config } from "../../../config/config.js";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName(`${config.bot_settings.slash_command_prefix}-help`)
+        .setDescription("Shows an overview of commands.")
+        .setDMPermission(false),
+    /**
+     * @param {import("discord.js").CommandInteraction} interaction
+     */
+    async execute(interaction){
+        const userCommands = /** @type {import("../../service/client.js").default} */ (interaction.client)
+            .commands.filter(cmd => cmd.data.default_member_permissions !== "4");
+
+        return await interaction.reply({
+            content: userCommands.map((cmd) => `**/${cmd.data.name}** - ${cmd.data.description}`).join("\n"),
+            ephemeral: true,
+        });
+    },
+};
