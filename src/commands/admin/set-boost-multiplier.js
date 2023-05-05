758d962660d22bbc9132bfc1ca5876202db68067
arellak <nimble0@protonmail.com>
2023-05-05 16:53:00 +0200

    added set boost multiplier command and locales

---
+++ src/commands/admin/set-boost-multiplier.js
@@ -0,0 +1,38 @@
+import path from "node:path";
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import { config } from "../../../config/config.js";
+import { QuickDB } from "quick.db";
+import __ from "../../service/i18n.js";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+const db = new QuickDB({
+    filePath: path.resolve("./data/guild_settings.sqlite"),
+});
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName(`${config.bot_settings.slash_command_prefix}-set-boost-mulitplier`)
+        .setDescription("Sets the boost multiplier for this server.")
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+        .addIntegerOption((option) =>
+            option.setName("value")
+                .setDescription("Server language")
+                .setRequired(true)),
+
+    /**
+     * @param {import("discord.js").CommandInteraction} interaction
+     */
+    async execute(interaction){
+        const value = interaction.options.get("value")?.value;
+        if (isNaN(value)) return await interaction.reply({ content: await __("errors.multiplier_nan")(interaction.guild), ephemeral: true });
+        if (value < 1) return await interaction.reply({ content: await __("errors.multiplier_too_small")(interaction.guildId), ephemeral: true });
+
+        await db.set(`guild-${interaction.guildId}.boost-multiplier`, value || 1);
+        return await interaction.reply({
+            content: await __("replies.multiplier_set", value)(interaction.guildId, true), ephemeral: true });
+    },
+};
