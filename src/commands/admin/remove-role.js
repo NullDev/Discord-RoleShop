26d2f7920d2f475e159606304c408109a7efa5a7
NullDev (Shadow) <chris@nulldev.org>
2023-05-09 19:22:27 +0200

    i18n for remove

--- src/commands/admin/remove-role.js
+++ src/commands/admin/remove-role.js
@@ -2,6 +2,7 @@ import path from "node:path";
 import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
 import { config } from "../../../config/config.js";
 import { QuickDB } from "quick.db";
+import __ from "../../service/i18n.js";
 
 // ========================= //
 // = Copyright (c) NullDev = //
@@ -32,21 +33,39 @@ export default {
      */
     async execute(interaction){
         const name = String(interaction.options.get("name")?.value);
-        if (!name) return await interaction.reply({ content: "Invalid name", ephemeral: true });
+        if (!name){
+            return await interaction.reply({
+                content: await __("replies.remove_role.invalid_role_name")(interaction.guildId),
+                ephemeral: true,
+            });
+        }
 
         const del = Boolean(interaction.options.get("delete")?.value || false);
 
         const role = interaction.guild?.roles.cache.find((r) => r.name === name);
-        if (!role) return await interaction.reply({ content: "Role not found", ephemeral: true });
+        if (!role){
+            return await interaction.reply({
+                content: await __("replies.remove_role.role_not_found", name)(interaction.guildId),
+                ephemeral: true,
+            });
+        }
 
         await db.delete(`guild-${interaction.guildId}.${role?.id}`);
 
         if (del){
-            await role.delete().catch(() => {
-                return interaction.reply({ content: "Failed to delete role", ephemeral: true });
+            await role.delete().catch(async() => {
+                return interaction.reply({
+                    content: await __("replies.remove_role.failed_to_remove", name)(interaction.guildId),
+                    ephemeral: true,
+                });
             });
         }
 
-        return await interaction.reply({ content: "Role removed", ephemeral: true });
+        return await interaction.reply({
+            content: del
+                ? await __("replies.remove_role.removed_server", name)(interaction.guildId)
+                : await __("replies.remove_role.removed", name)(interaction.guildId),
+            ephemeral: true,
+        });
     },
 };
