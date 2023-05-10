1cf73922827960004eb5dd0707d07897acebf178
NullDev (Shadow) <chris@nulldev.org>
2023-05-10 21:41:04 +0200

    merged interaction handlers and removed buy command

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,66 @@
+import Log from "../util/log.js";
+import __ from "../service/i18n.js";
+import buyEventHandler from "../service/buyService.js";
+import returnEventHandler from "../service/returnService.js";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+/**
+ * Handle command Interaction events
+ *
+ * @param {import("discord.js").ChatInputCommandInteraction} interaction
+ * @return {Promise<void>}
+ */
+const handleCommandInteraction = async function(interaction){
+    const command = /** @type {import("../service/client.js").default} */ (interaction.client)
+        .commands.get(interaction.commandName);
+
+    if (!command){
+        Log.warn(`No command matching ${interaction.commandName} was found.`);
+        await interaction.reply({ content: await __("errors.command_not_found", interaction.commandName)(interaction.guildId), ephemeral: true });
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    }
+    catch (error){
+        Log.error("Error during command execution: ", error);
+        if (interaction.replied || interaction.deferred){
+            await interaction.followUp({ content: await __("errors.generic_command_execution_failed")(interaction.guildId), ephemeral: true });
+        }
+        else {
+            await interaction.reply({ content: await __("errors.generic_command_execution_failed")(interaction.guildId), ephemeral: true });
+        }
+    }
+};
+
+/**
+ * Handle select menu Interaction events
+ *
+ * @param {import("discord.js").StringSelectMenuInteraction} interaction
+ * @return {Promise<void>}
+ */
+const handleSelectMenuInteraction = async function(interaction){
+    if (interaction.isStringSelectMenu()){
+        if (interaction.customId === "role_buy") await buyEventHandler(interaction);
+        else if (interaction.customId === "role_return") await returnEventHandler(interaction);
+
+        else Log.warn(`No select menu matching ${interaction.customId} was found.`);
+    }
+};
+
+/**
+ * Handle interactionCreate event
+ *
+ * @param {import("discord.js").Interaction} interaction
+ * @return {Promise<void>}
+ */
+const interactionCreateHandler = async function(interaction){
+    if (interaction.isStringSelectMenu()) await handleSelectMenuInteraction(interaction);
+    if (interaction.isChatInputCommand()) await handleCommandInteraction(interaction);
+};
+
+export default interactionCreateHandler;
