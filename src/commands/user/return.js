55689b768b6c2c5210ffb392dd291ccd7050e521
NullDev (Shadow) <chris@nulldev.org>
2023-05-11 19:23:05 +0200

    finished return service

--- src/commands/user/return.js
+++ src/commands/user/return.js
@@ -1,5 +1,73 @@
+import path from "node:path";
+import { ComponentType, SlashCommandBuilder } from "discord.js";
+import { QuickDB } from "quick.db";
+import { config } from "../../../config/config.js";
+import __ from "../../service/i18n.js";
+
 // ========================= //
 // = Copyright (c) NullDev = //
 // ========================= //
 
-export default {};
+const roleDb = new QuickDB({
+    filePath: path.resolve("./data/roles.sqlite"),
+});
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName(`${config.bot_settings.slash_command_prefix}-return`)
+        .setDescription("Return a Role")
+        .setDMPermission(false),
+
+    /**
+     * @param {import("discord.js").CommandInteraction} interaction
+     */
+    async execute(interaction){
+        await interaction.deferReply();
+
+        const roles = await roleDb.get(`guild-${interaction.guildId}`);
+        if (!roles || Object.keys(roles).length === 0){
+            return await interaction.reply({
+                content: await __("errors.no_roles")(interaction.guildId),
+                ephemeral: true,
+            });
+        }
+
+        const userRoles = /** @type {import("discord.js").GuildMemberRoleManager} */ (interaction.member?.roles).cache;
+        const commonRoles = Object.keys(roles).filter(roleid => userRoles.has(roleid));
+
+        if (commonRoles.length === 0){
+            return await interaction.reply({
+                content: await __("errors.no_roles")(interaction.guildId),
+                ephemeral: true,
+            });
+        }
+
+        const rolesToReturn = commonRoles.map(roleid => {
+            const roleObj = interaction.guild?.roles.cache.get(roleid);
+            const role = roleObj?.name;
+            const price = roles[roleid];
+            return [role, roleid, price];
+        });
+
+        const selectMenu = {
+            customId: "role_return",
+            placeholder: "Select a role to return",
+            options: rolesToReturn.map(([role, roleid, price]) => ({
+                label: role,
+                value: JSON.stringify({ role, roleid, price }),
+                description: `Return this role. The original price was ${price} points.`,
+            })),
+        };
+
+        return await interaction.followUp({
+            content: "Which role would you like to return?",
+            components: [{
+                type: ComponentType.ActionRow,
+                components: [{
+                    type: ComponentType.StringSelect,
+                    ...selectMenu,
+                }],
+            }],
+        });
+    },
+};
--- src/service/returnService.js
+++ src/service/returnService.js
@@ -1,3 +1,7 @@
+import createYesNoInteraction from "../events/yesNoInteraction.js";
+import logTransaction from "./transactionLog.js";
+import __ from "./i18n.js";
+
 // ========================= //
 // = Copyright (c) NullDev = //
 // ========================= //
@@ -5,11 +9,34 @@
 /**
  * Handle Return event
  *
- * @param {import("discord.js").Interaction} interaction
+ * @param {import("discord.js").StringSelectMenuInteraction} interaction
  * @return {Promise<void>}
  */
 const returnEventHandler = async function(interaction){
-    console.log("returnEventHandler", interaction.id);
+    const { role, roleid, price } = JSON.parse(interaction.values[0]);
+
+    createYesNoInteraction(interaction, {
+        promptText: `so u wanna return ${role}? You will NOT get the ${price} points back?`,
+    }).then(async(answer) => {
+        if (answer === "yes"){
+            const rObj = /** @type {import("discord.js").GuildMemberRoleManager} */ (interaction.member?.roles);
+
+            try {
+                await rObj.remove(roleid);
+            }
+            catch (e){
+                return await interaction.channel?.send({ content: "went wrong." });
+            }
+
+            await logTransaction(interaction.guildId, interaction.user.id, roleid, role, "RETURN");
+            return await interaction.channel?.send({ content: "u returned thingy" });
+        }
+        else if (answer === "no"){
+            return await interaction.channel?.send({ content: await __("generic.aborted")(interaction.guildId) });
+        }
+
+        return null;
+    });
 };
 
 export default returnEventHandler;
